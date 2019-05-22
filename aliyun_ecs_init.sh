0e62d8e45cbe542af946232f3f76ce700e543731
tengattack <tengattack@gmail.com>
2019-05-22 17:39:52 +0800

    Update aliyun ecs init install nfs-utils

--- aliyun_ecs_init.sh
+++ aliyun_ecs_init.sh
@@ -89,7 +89,7 @@ sudo mv ~/.tmux.conf /etc/tmux.conf
 # reference: https://docs.docker.com/install/linux/docker-ce/centos/
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
-  lvm2
+  lvm2 nfs-utils
 sudo yum-config-manager \
   --add-repo \
   https://download.docker.com/linux/centos/docker-ce.repo
